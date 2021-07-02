4db6667acda855bd80fc75ccfeb2851a2cb9a585
Ђорђе <37450617+Djolevil@users.noreply.github.com>
2021-07-02 23:04:47 +0200

    Move list of expenses into new ExpensesList component with conditional return statements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter'
 import './Expenses.css'
@@ -14,18 +14,7 @@ const Expenses = (props) => {
   const filteredExpenses = props.items.filter(expense => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
-  let expensesContent = <p>No expenses found.</p>
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id} 
-        title={expense.title} 
-        amount={expense.amount} 
-        date={expense.date} 
-      />
-     ))
-  }
+  
 
     return (
      <div>
@@ -34,7 +23,7 @@ const Expenses = (props) => {
       selected={filteredYear}
       onChangeFilter={filterChangeHandler}
     />
-     {expensesContent}
+     <ExpensesList items={filteredExpenses} />
   </Card>
   </div>
     );
